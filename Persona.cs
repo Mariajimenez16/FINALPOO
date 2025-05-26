344999e2311182229007871f84dc54d24307fac5
Natalia Giraldo Morales <natalia.giraldom.col@upb.edu.co>
2025-05-26 14:51:20 -0500

    Clases del Match

---
+++ Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalPOO
+{
+    public class Persona
+    {
+        public string Nombre;
+        public int Edad;
+        public string Ubicacion;
+        public string Genero;
+        public Image Foto;
+
+        public Persona(string nombre, int edad, string ubicacion, string genero, Image foto)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Ubicacion = ubicacion;
+            Genero = genero;
+            Foto = foto;
+        }
+    }
+}
